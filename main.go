e44c969912acfe65eaf42a7a6afb000c29c37ff3
ko44d <koyoshid622@gmail.com>
2024-04-02 10:35:27 +0900

    Server型とMux型を作成して仕様変更。またDockerfileの間違いを修正

--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/ko44d/go-api-sample/config"
-	"golang.org/x/sync/errgroup"
 	"log"
 	"net"
-	"net/http"
 	"os"
 )
 
@@ -31,24 +29,7 @@ func run(ctx context.Context) error {
 
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-
-	s := http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
--- server_test.go
+++ server_test.go
@@ -10,7 +10,8 @@ import (
 	"testing"
 )
 
-func TestRun(t *testing.T) {
+func TestServer_Run(t *testing.T) {
+
 	t.Skip("run refactoring")
 
 	l, err := net.Listen("tcp", "localhost:0")
@@ -20,8 +21,13 @@ func TestRun(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
+
 	eg.Go(func() error {
-		return run(ctx)
+		s := NewServer(l, mux)
+		return s.Run(ctx)
 	})
 
 	in := "message"
