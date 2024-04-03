2e2e8325b668bc4ea6b67fc89dd566f0427aa755
ko44d <koyoshid622@gmail.com>
2024-04-03 23:00:35 +0900

    テーブル定義が間違っていたので修正

--- _ddl/task.sql
+++ _ddl/task.sql
@@ -3,7 +3,7 @@ CREATE TABLE `task`
     `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'タスクの識別子',
     `title`    VARCHAR(128) NOT NULL COMMENT 'タスクのタイトル',
     `status`   VARCHAR(20)  NOT NULL COMMENT 'タスクの状態',
-    `created`  VARCHAR(6)   NOT NULL COMMENT 'レコード作成日時',
-    `modified` VARCHAR(6)   NOT NULL COMMENT 'レコード修正日時',
+    `created`  DATETIME(6)   NOT NULL COMMENT 'レコード作成日時',
+    `modified` DATETIME(6)   NOT NULL COMMENT 'レコード修正日時',
     PRIMARY KEY (`id`)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='タスク';
