af14996eff781870d287b840e4106605a0d7650a
ko44d <koyoshid622@gmail.com>
2024-04-03 17:28:13 +0900

    DB使うための処理などを追加

--- config/config.go
+++ config/config.go
@@ -5,8 +5,13 @@ import (
 )
 
 type Config struct {
-	Env  string `env:"TODO_ENV" envDefault:"dev"`
-	Port int    `env:"PORT" envDefault:"80"`
+	Env        string `env:"TODO_ENV" envDefault:"dev"`
+	Port       int    `env:"PORT" envDefault:"80"`
+	DBHost     string `env:"DB_HOST" envDefault:"127.0.0.1"`
+	DBPort     int    `env:"DB_PORT" envDefault:"33306"`
+	DBUser     string `env:"DBUser" envDefault:"todo"`
+	DBPassword string `env:"DBPassword" envDefault:"todo"`
+	DBName     string `env:"DBName" envDefault:"todo"`
 }
 
 func New() (*Config, error) {
--- go.mod
+++ go.mod
@@ -8,9 +8,12 @@ require (
 )
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/go-playground/validator/v10 v10.19.0
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/go-cmp v0.6.0
+	github.com/jmoiron/sqlx v1.3.5
 )
 
 require (
